6c29537bffda95cc7dff0152a67882b1d500145a
Ahbab Shahin Chowdhury <ahbabshahin7788@gmail.com>
2025-02-02 15:20:53 +0600

    App js updated

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
 	try {
-		await connectDB(getMongoUri());
+		await connectDB(process.env.MONGO_URI);
 		app.listen(port, console.log(`Server is listening on port: ${port}`));
 	} catch (error) {
 		console.log(error);
